9ae7b5efc1eb453601b40f36ec66d9ea44a76832
aasiagithup <132499880+aasiagithup@users.noreply.github.com>
2023-07-04 15:19:14 -0700

    1 file modified

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 let root = document.getElementById("root");
 
+let container = ReactDOM.createRoot(root);
+let name = "asia";
+container.render(
+<div>
+    {name}
+    {1234556}
+    {(name ==="Aasia") ? <h1>hello</h1> : <h1> "m</h1>}
+    <h1>
+      hello world
+    </h1>
+    <button>Aasia</button>
+  </div>,
+)
